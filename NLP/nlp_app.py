c3aa362404de99f8f4ed00515b9356cd573a3762
yakoyma <96658352+yakoyma@users.noreply.github.com>
2025-01-30 23:27:50 +0100

    First commit

---
+++ NLP/nlp_app.py
@@ -0,0 +1,92 @@
+"""
+===============================================================================
+Multiclass Classification Project: Sentiment Analysis using a Natural
+Language Processing (NLP) Classifier and Gradio application
+===============================================================================
+
+This file is organised as follows:
+Prediction using Gradio application
+"""
+# Standard libraries
+import platform
+import warnings
+
+# Suppress warnings
+warnings.filterwarnings('ignore')
+
+# Other libraries
+import transformers
+import nltk
+import gradio as gr
+
+
+from transformers import (AutoTokenizer,
+                          AutoModelForSequenceClassification,
+                          pipeline)
+from nltk import word_tokenize
+
+
+# Display versions of platforms and packages
+print('\nPython: {}'.format(platform.python_version()))
+print('Transformers: {}'.format(transformers.__version__))
+print('NLTK: {}'.format(nltk.__version__))
+print('Gradio: {}'.format(gr.__version__))
+
+
+
+"""
+===============================================================================
+Prediction using Gradio application
+===============================================================================
+"""
+def get_prediction(text: str):
+    """This function predicts the sentiment of text using NLP Classifier.
+
+    Args:
+        text (str): the user input
+
+    Returns:
+        response (str): the predicted sentiment and the score of prediction
+    """
+
+    # Encode the text
+    text = text.encode(encoding='utf-8').decode(encoding='utf-8')
+
+    # Instantiate the model
+    model_path = 'tabularisai/multilingual-sentiment-analysis'
+    tokenizer = AutoTokenizer.from_pretrained(
+        pretrained_model_name_or_path=model_path)
+    model = AutoModelForSequenceClassification.from_pretrained(
+        pretrained_model_name_or_path=model_path)
+    pipe = pipeline(
+        task='text-classification',
+        model=model,
+        tokenizer=tokenizer
+    )
+    print(f'\nText length with the classifier '
+          f'Tokenizer: {len(tokenizer.tokenize(text))}')
+
+    if len(tokenizer.tokenize(text)) <= 512:
+        score = round(pipe(text)[0]['score'], 2)
+        response = (f'The number of words in the text is '
+                    f'{len(tokenizer.tokenize(text))}.'
+                    + '\nThe predicted sentiment is: ' + pipe(text)[0]['label']
+                    + f'.\nThe score of prediction is {score}.')
+    else:
+        response = (f'The number of words in the text is '
+                    f'{len(word_tokenize(text))}. The text is too long.')
+    return response
+
+
+# Instantiate the app
+app = gr.Interface(
+    fn=get_prediction,
+    inputs='text',
+    outputs='text',
+    title='Sentiment Analysis'
+)
+
+
+
+if __name__ == '__main__':
+    app.launch()
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# sentiment_analysis_prediction_app
+# Project: Sentiment Analysis using an API application
+
+The purpose of this Multiclass Classification project is to implement an application to predict the sentiment of a text.
+The various approaches implemented:
+- General sentiment analysis using an Artificial Intelligence (AI) model.
+- General sentiment analysis using a Natural Language Processing (NLP) model specialising in multiclass classification.
+- Specific sentiment analysis of recipe reviews and user feedback using a Machine Learning model trained on the following dataset: [Recipe Reviews and User Feedback](https://archive.ics.uci.edu/dataset/911/recipe+reviews+and+user+feedback+dataset/)
+
+
+# Projet : Analyse des sentiments à l'aide d'une application API
+
+Ce projet de classification multi-classes consiste à implémenter une application permettant de prédire le sentiment d'un text.
+Les différentes approches implémentées :
+- L'analyse générale des sentiments avec un modèle d'intelligence artificielle (IA).
+- L'analyse générale des sentiments avec un modèle de traitement de langage naturel (NLP) spécialisé dans la classification multi-classes.
+- L'analyse spécifique des sentiments de critiques des recettes et commentaires des utilisateurs avec un modèle de Machine Learning (ML) entraîné sur le jeu de données suivant : [Recipe Reviews and User Feedback](https://archive.ics.uci.edu/dataset/911/recipe+reviews+and+user+feedback+dataset/) 
