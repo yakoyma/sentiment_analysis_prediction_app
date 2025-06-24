40b3d164504a2aab9797af41bc1d2efc1629ec1f
yakoyma <96658352+yakoyma@users.noreply.github.com>
2025-06-25 01:02:12 +0200

    First commit
    Signed-off-by: yakoyma <96658352+yakoyma@users.noreply.github.com>

---
+++ ML/ml_pycaret_app.py
@@ -0,0 +1,120 @@
+"""
+===============================================================================
+Multiclass Classification Project: Sentiment Analysis Application of Cooking
+Recipe Reviews and User Feedback with a Machine Learning (ML) model and Gradio
+===============================================================================
+"""
+# Standard library
+import platform
+
+# Other libraries
+import numpy as np
+import pandas as pd
+import spacy
+import demoji
+import pycaret
+import gradio as gr
+
+
+from demoji import replace
+from pycaret.classification import *
+
+
+# Display versions of platforms and packages
+print('\nPython: {}'.format(platform.python_version()))
+print('NumPy: {}'.format(np.__version__))
+print('Pandas: {}'.format(pd.__version__))
+print('SpaCy: {}'.format(spacy.__version__))
+print('Demoji: {}'.format(demoji.__version__))
+print('PyCaret: {}'.format(pycaret.__version__))
+print('Gradio: {}'.format(gr.__version__))
+
+
+
+def get_prediction(max_tokens_length: int, text: str) -> str:
+    """This function predicts the sentiment of a text using a trained ML model.
+
+    Args:
+        max_tokens_length (int): the maximum number of text tokens
+        text (str): the user's text
+
+    Returns:
+        response (str): the predicted sentiment
+    """
+
+    try:
+
+        # Check wether the user input is valid
+        if max_tokens_length > 0 and text:
+
+            # Instantiate the NLP model
+            nlp = spacy.load(name='xx_ent_wiki_sm')
+            nlp.add_pipe(factory_name='sentencizer')
+
+            # Cleanse the text
+            text = text.strip()
+            text = replace(string=text, repl='')
+
+            # Check if there is any text and wether the text tokens length at
+            # the model input is less than the maximum tokens limit selected
+            text_tokens_length = len(nlp(text))
+            if (text_tokens_length > 0 and
+                text_tokens_length < max_tokens_length):
+
+                # Create dataset with the text data
+                X = pd.DataFrame(data={'text': [text]})
+
+                # Load the model
+                model = load_model(model_name='models/pycaret/model')
+
+                # Make prediction
+                prediction = model.predict(X)
+
+                # Display the sentiment
+                sentiments = {
+                    'Neutral': 0,
+                    'Very dissatisfied': 1,
+                    'Dissatisfied': 2,
+                    'Correct': 3,
+                    'Satisfied': 4,
+                    'Very satisfied': 5
+                }
+                sentiment = next(
+                    key for key, value in sentiments.items() if
+                    prediction[0] == value
+                )
+                response = (f'The predicted sentiment is: {sentiment}.')
+            else:
+                response = ('The text is too long and the maximum number of '
+                            'tokens has been exceeded, or the text is '
+                            'unreadable.')
+        else:
+            response = ('Invalid input data. Please complete the field '
+                        'correctly.')
+
+    except Exception as error:
+        response = f'The following unexpected error occurred: {error}'
+    return response
+
+
+
+# Instantiate the app
+app = gr.Interface(
+    fn=get_prediction,
+    inputs=[
+        gr.Slider(
+            minimum=0,
+            maximum=10000,
+            step=1000,
+            label='Maximum text length'
+        ),
+        gr.Textbox(label='Text')
+    ],
+    outputs=gr.Textbox(label='Sentiment'),
+    title='Recipe Reviews and User Feedback Sentiment Analysis Application'
+)
+
+
+
+if __name__ == '__main__':
+    app.launch()
